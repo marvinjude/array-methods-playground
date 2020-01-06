276835dc5ef173975ac566b17097667a5b9f5dfb
Jude Agboola <marvinjudehk@gmail.com>
2020-01-06 20:33:11 +0100

    refactory ._map

--- src/map.js
+++ src/map.js
@@ -1,17 +1,15 @@
-//_<METHOD_NAME>
-
 /**
- * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+ * Calls a defined callback function on each element of an array, and returns an array that contains returned values on each iteration.
  * @param {function} callback - callback will be called with current element, index and the array in concern on every iteration
  * @param {Object} thisArg -  An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
  */
-Array.prototype._map = function(callbackFn, thisArg = this) {
+Array.prototype._map = function(callbackFn, thisArg) {
   if (typeof callbackFn != "function")
     throw new Error(`TypeError: ${callbackFn} is not a function`);
 
   let newArray = [];
-  for (let index = 0; index < thisArg.length; index++) {
-    const newElement = callbackFn(thisArg[index], index, thisArg);
+  for (let index = 0; index < this.length; index++) {
+    const newElement = callbackFn.call(thisArg, this[index], index, this);
     newArray = [...newArray, newElement];
   }
   return newArray;
